b6e7f303aa0d0c154477b89d1636750eb38abd53
Dimitri M <ibmiti@gmail.com>
2022-01-10 13:16:41 -0500

    sending latest javascript notes

--- javascriptNotes.js
+++ javascriptNotes.js
@@ -11,7 +11,6 @@
 //         2. e.g : 
 //             let x = function(){
 //                 if (true) {
-
 //                     console.log(v)
 //                     console.log(l)
 //                     var v = 2
@@ -24,20 +23,50 @@
 //     a : therefore var is more robust.
 //     b : var is accepted as proper syntax within older browsers ( pre-es6 )
 
-
 // 5. diff between : = AND == AND === 
 //     a: = is to set an value to a key or var/let 
 //     b: == is to perform an cursory comparison between values '1' == 1 => true
 //     c: === is a strict compare, e.g : 
 //             1 === 1 => true, 1 === '1' => false
 
-
 // 6. const : 
 //     a : disallows reassignment of value from variable name. e.g: 
-
 //             const foo = 'bar';
 //             foo = 'jaz' => false 
 //             ...
 //             const foo = [1,2,3]
 //             foo.push(4) => true => foo[1,2,3,4]
         
+
+// 7. how to loop through javascript object :
+
+    // e.g : 
+
+        var object_a = { "one": 1, "two": 2, "three": 3 };
+
+        for (var key in object_a) { 
+            if (object_a.hasOwnProperty(key)) {
+                console.log(key + " -> " + object_a[key]);
+            }
+        }
+
+    // e.g 2 : 
+
+        for (var key of Object.keys(object_a)) {
+            console.log(key + " -> " + object_a[key])
+        }
+
+        e.g 3 : ( not supported using internetexplorer ) : 
+
+            for (let [key, value] of object.entries(object_a)){
+                console.log(`${key}: ${value}`); // usage of template literal : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+            }
+
+
+
+// 8. javascript closures : 
+
+// 1. a closure is the combination of : a functions scope + a reference to the functions outerscope ( the global lexical env )
+
+            // a : lexical_env => the portion of the code in which has reference to the functions entire body; from the function name ( definition ), to its collection of variables_names/values.
+
